3a3a4e62f5f8194fb6e89ae102169b327e6547d9
Igor Nikolaev <tamarisk60@yandex.ru>
2024-04-12 12:05:57 +0200

    lesson 10

---
+++ lesson_10/HW/index.js
@@ -0,0 +1,19 @@
+// TASK 1
+
+let myNumber = 100/(25%3);
+console.log(myNumber);
+
+// Task 2
+
+let myString = "Hello" + "world" + "!" + 2024;
+console.log(myString);
+
+// Task 3
+
+let is42 = (42 == `42`);
+console.log(is42);
+
+// Task 4
+
+myNumber = parseInt("123smth");
+console.log(myNumber);
---
+++ lesson_10/index.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+let x = "hello";
+x = "ogo";
+let y = "World";
+y = "go"
+console.log(x+y);
+//
